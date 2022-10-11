2eb9d70c626521d5c6cccd4b44524a83b7c62edb
unknown <loreta.padaigaite@codeacademylt.onmicrosoft.com>
2022-10-11 20:01:18 +0300

    3paskaitos darbas

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import logo from './logo.svg';
-import { Test } from './Test/test';
-import { Button } from './Button/Button';
-import { Header } from './Header/Header';
+import { Greeting } from './Greeting/Greeting';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-      <Header/>
-
-      < Test/>
-
-      < Button className='button' text='i am first button'/>
-      < Button>Click my <span>123</span></Button>
-      < Button text='Press'/>
-
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <Greeting isLoggedIn={false} unreadMessages={[1, 1, 1]} />
     </div>
   );
 }

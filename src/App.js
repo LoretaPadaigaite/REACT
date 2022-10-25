22fa330496bfce05cc71dabadd244a3df70a9450
unknown <loreta.padaigaite@codeacademylt.onmicrosoft.com>
2022-10-25 18:56:58 +0300

    6paskaita pirma dalis

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 
-import { Counter } from './Counter/Counter';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   
-const [isCounterVisible, setIsCounterVisible] = useState(true);
-const handelToggleCounter = () => {
-  setIsCounterVisible(!isCounterVisible);
-}
+  useEffect(() => {
+    console.log('COMPONENT Mounted')
+  }, [count]);
+
+  const handleIncrement = (event) => {
+    console.log(event)
+    setCount(count + 1)
+  }
+  const handleIncrementKeyDown = (event) => {
+    console.log('KEY DOWN')
+    console.log(event)
+  }
+  const handleIncrementKeyUp = (event) => {
+    console.log('KEY UP')
+    console.log(event)
+  }
+
+  const handleInputChange = (event) => {
+    console.log('INPUT Value CHANGE')
+    console.log(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('SUBMIT')
+  }
 
   return (
     <div className="App">
 
+      <form onSubmit={handleSubmit}>
+      {count}
+      <button 
+        onKeyUp={handleIncrementKeyUp} 
+        onKeyDown={handleIncrementKeyDown} 
+        onClick={handleIncrement}
+      >
+        Increment
+      </button>
 
-    <button onClick={handelToggleCounter}>Toggle Counter</button>
+      <input onChange={handleInputChange} />
 
-     {isCounterVisible &&  <Counter/>}
+</form>
   
     </div>
   );

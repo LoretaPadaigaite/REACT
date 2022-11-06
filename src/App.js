ae77ae99075f8ec3898b250e7fac7874f7d68049
unknown <loreta.padaigaite@codeacademylt.onmicrosoft.com>
2022-11-07 01:10:26 +0200

    9paskaita

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import { HomePage } from './views/HomePage/HomePage';
 import {AboutPage} from './views/AboutPage/AboutPage';
 import { ContactsPage } from './views/ContactsPage/ContactsPage';
 import {Navigation} from './components/Navigation/Navigation'
+import {PageLayout} from './components/PageLayout/PageLayout';
+import { LoginPage } from './views/LoginPage/LoginPage';
+import { useState } from 'react';
 
 
 function App() {
 
+  const [user, setUser] = useState(null);
+
+  const handleLogin = () => setUser({ username: 'Petras123' });
+  const handleLogout = () => setUser(null);
+
   return (
     <div className="App">
-
-      <Navigation/>
-
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/about' element={<AboutPage/>} />
-        <Route path='/contacts' element={<ContactsPage/>}/>
+        <Route path="/" element={<PageLayout user={user} onLogout={handleLogout} />}>
+          <Route index element={<HomePage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+        </Route>
+        {/* Login route */}
+        <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
       </Routes>
-
     </div>
   );
 }
